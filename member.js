79e31c8ce76748211e124ceac891549763765353
Orbit4l <31424959+Orbit4l@users.noreply.github.com>
2023-07-22 12:53:05 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,1 +1,15 @@
+function skillsMember() {
+  const member = document.querySelector('.member');
+  const memberSkills = document.querySelector('.member__skills');
+  const memberSkillsClose = document.querySelector('.member__skills-close');
 
+  if (member) {
+    member.addEventListener('click', () => {
+      memberSkills.classList.add('member__skills--active');
+    });
+
+    memberSkillsClose.addEventListener('click', () => {
+      memberSkills.classList.remove('member__skills--active');
+    });
+  }
+}
